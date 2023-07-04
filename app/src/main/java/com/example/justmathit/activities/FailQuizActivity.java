0a06e12cf84294d5bd91b939e55256365722b208
GeorgeCR7 <gasdextercr7@yahoo.com>
2023-07-04 17:57:05 +0300

    Success & failure actvs fully completed.

--- app/src/main/java/com/example/justmathit/activities/FailQuizActivity.java
+++ app/src/main/java/com/example/justmathit/activities/FailQuizActivity.java
@@ -2,15 +2,28 @@ package com.example.justmathit.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.justmathit.R;
 
 public class FailQuizActivity extends AppCompatActivity {
 
+    Button btnFailOKBackMain;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fail_quiz);
+
+        btnFailOKBackMain = findViewById(R.id.btnFailOKBackMain);
+
+        btnFailOKBackMain.setOnClickListener(view -> {
+            Intent intent = new Intent(FailQuizActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
--- app/src/main/java/com/example/justmathit/activities/SuccessQuizActivity.java
+++ app/src/main/java/com/example/justmathit/activities/SuccessQuizActivity.java
@@ -2,15 +2,27 @@ package com.example.justmathit.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.justmathit.R;
 
 public class SuccessQuizActivity extends AppCompatActivity {
 
+    Button btnOKBackMain;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success_quiz);
+
+        btnOKBackMain = findViewById(R.id.btnOKBackMain);
+
+        btnOKBackMain.setOnClickListener(view -> {
+            Intent intent = new Intent(SuccessQuizActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
